9857147ef2539ec44a295149ad1dcddbb5a47250
MirZa <152170963+theycallmeprofessor@users.noreply.github.com>
2024-10-28 04:16:58 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
+SESSION_ID: process.env.SESSION_ID || "SYNTDCRT#5WS9MPQcBrx7j9u4YdtttHGduqrHdFWPQYhCHRzNYiM",
 MONGODB: process.env.MONGODB || "enter mongodb url",
 };
